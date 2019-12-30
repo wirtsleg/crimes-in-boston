1ede3e096a0cc89a1d956bc29be4ef3d1055f8f0
shuto4ka <noga?1virta>
2019-12-30 14:21:13 +0300

    Implement selects

--- src/main/scala/com/github/mrpowers/my/cool/project/CrimeInBoston.scala
+++ src/main/scala/com/github/mrpowers/my/cool/project/CrimeInBoston.scala
@@ -1,11 +1,11 @@
 package com.github.mrpowers.my.cool.project
 
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
-import org.json4s.{DefaultFormats, Formats}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 case class OffenceCode(CODE: String, NAME: String)
+
+case class CrimeTypeRow(district: String, crime_type: String, count: Long)
 
 object CrimeInBoston {
 
@@ -13,22 +13,61 @@ object CrimeInBoston {
     val spark = SparkSession.builder().master("local[*]").appName("crime_in_boston").getOrCreate()
     import spark.implicits._
 
-    //    val crimes = spark.read.csv(args(0))
+    val crimes = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(args(0))
+
     val offenceCodes = spark.read
       .option("header", "true")
       .option("inferSchema", "true")
-      .csv("C:/offense_codes.csv" /*args(1)*/)
+      .csv(args(1))
       .as[OffenceCode]
-      .map(row => OffenceCode("%05d".format(row.CODE.toInt), row.NAME))
-      .show()
-    //    val outputFile = args(2)
+      .map(row => OffenceCode(
+        "%05d".format(row.CODE.toInt),
+        row.NAME.replaceAll("\"", "").split(" - ")(0))
+      )
 
+    val fullCrimeTable = crimes
+      .join(broadcast(offenceCodes), crimes("OFFENSE_CODE") === offenceCodes.col("CODE"))
+      .filter($"DISTRICT" =!= "U")
+      .withColumnRenamed("DISTRICT", "district")
+      .withColumnRenamed("NAME", "crime_type")
+      .withColumnRenamed("INCIDENT_NUMBER", "incident_number")
+      .cache()
+
+    val crimesTotalAvgLatLong = fullCrimeTable
+      .groupBy($"district")
+      .agg(
+        count("incident_number").as("crimes_total"),
+        avg("Lat").as("lat"),
+        avg("Long").as("lng")
+      )
+      .withColumn("crimes_monthly", round($"crimes_total" / 12, 2))
+
+    val crimeTypes = fullCrimeTable
+      .select("incident_number", "district", "crime_type")
+      .groupBy("district", "crime_type")
+      .count()
+      .as[CrimeTypeRow]
+      .groupByKey(_.district)
+      .mapGroups {
+        case (row, iter) =>
+          val types = iter.toList.sortBy(-_.count).take(3)
+            .map(_.crime_type)
+            .mkString(", ")
+          CrimeTypeRow(row.distinct, types, 0)
+      }
+      .withColumnRenamed("crime_type", "frequent_crime_types")
+      .select("district", "frequent_crime_types")
+
+    val result = crimeTypes
+      .join(crimesTotalAvgLatLong, crimeTypes("district") === crimesTotalAvgLatLong("district"))
+      .select(crimeTypes("district"), $"crimes_total", $"crimes_monthly", $"frequent_crime_types", $"lat", $"lng")
+
+    result.repartition(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .parquet(args(2))
   }
-
-  //  crimes
-  //    .join(offenceCodes, crimes("OFFENSE_CODE") === offenceCodes.col("code"))
-  //    .show()
-  //    ..map(json => parse(json).extract[Wine])
-  //    .foreach(wine => println(wine))
-
 }
